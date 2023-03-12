eba4c99c4a98c22a84f3bc5ba046d98e50ad7c20
ahmed 3amer <ahmednsereldin@gmail.com>
2023-03-12 04:16:07 +0200

    ColumnarEn\Decrypt

--- Milestone1and2/SecurityPackage[Template]/securitylibrary/MainAlgorithms/Columnar.cs
+++ Milestone1and2/SecurityPackage[Template]/securitylibrary/MainAlgorithms/Columnar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,17 +11,95 @@ namespace SecurityLibrary
     {
         public List<int> Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            List<int> key = new List<int>();
+
+
+
+            
+            return key;
         }
 
         public string Decrypt(string cipherText, List<int> key)
         {
-            throw new NotImplementedException();
+           // throw new NotImplementedException();
+            string plainText = "";
+            
+            int numOfCols = key.Count;
+
+            int numOfRows = (int)Math.Ceiling((double)cipherText.Length / numOfCols);
+
+            char[,] matrix = new char[numOfRows, numOfCols];
+
+            int[] arr = new int[numOfCols];
+
+            for (int i = 0; i < key.Count; i++)
+            {
+                arr[key[i] /* 1-base */ - 1] = i;
+            }
+
+            int index = 0;
+            
+            for(int i = 0; i < numOfCols; i++)
+            {
+                for(int j = 0; j < numOfRows; j++)
+                {
+                    matrix[j, arr[i]] = index < cipherText.Length ? cipherText[index++] : 'x';
+                }
+            }
+
+
+
+            for (int i = 0; i < numOfRows; i++)
+            {
+                for (int j = 0; j < numOfCols; j++)
+                {
+                    //if (matrix[i, j ]!= '*')
+                        plainText += matrix[i, j];
+                }
+            }
+
+            return plainText.ToLower();
         }
 
         public string Encrypt(string plainText, List<int> key)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            string cipherText = "";
+
+            int numOfCols = key.Count;
+
+            int numOfRows = (int) Math.Ceiling((double) plainText.Length / numOfCols);
+
+            char[,] matrix = new char[numOfRows, numOfCols];
+
+            int index = 0;
+            for(int i = 0; i < numOfRows; i++)
+            {
+                for(int j = 0; j < numOfCols; j++)
+                {
+                    matrix[i, j] = index < plainText.Length ? plainText[index++] : 'x';
+                }
+            }
+
+            int[] arr = new int[numOfCols];
+
+            for (int i = 0; i < key.Count; i++)
+            {
+                arr[key[i] /* 1-base */ - 1] = i;
+            }
+
+
+            for (int i = 0; i < numOfCols; i++)
+            {
+                for(int j = 0; j < numOfRows; j++)
+                {
+                    /*if(matrix[j, arr[i]] != '*')*/ cipherText += matrix[j, arr[i]];
+                }
+            }
+
+            return cipherText.ToUpper();
         }
     }
 }
