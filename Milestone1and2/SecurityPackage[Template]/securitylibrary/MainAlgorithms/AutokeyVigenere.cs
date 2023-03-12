d56c0af02d9dffe63b5a0fdf284db306f8d20f12
Mariamx811 <mariam.ahmed.xx24@gmail.com>
2023-03-12 02:42:35 +0200

    Encryption

--- Milestone1and2/SecurityPackage[Template]/securitylibrary/MainAlgorithms/AutokeyVigenere.cs
+++ Milestone1and2/SecurityPackage[Template]/securitylibrary/MainAlgorithms/AutokeyVigenere.cs
@@ -10,7 +10,7 @@ namespace SecurityLibrary
     {
         public string Analyse(string plainText, string cipherText)
         {
-            Console.WriteLine(plainText);
+            throw new NotImplementedException();
         }
 
         public string Decrypt(string cipherText, string key)
@@ -20,7 +20,36 @@ namespace SecurityLibrary
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            int index = 0;
+            string cipherText = "";
+            while(plainText.Length != key.Length)
+            {
+                key += plainText[index];
+                index++;
+            }
+
+            int[,] letters = new int[26,26];
+            for(int i = 0; i < 26; i++)
+            {
+                for(int j = 0; j < 26; j++)
+                {
+                    letters[i, j] = (int)'A' + (i + j) % 26;
+                }
+            }
+
+            int Key_counter = 0;
+            key = key.ToUpper();
+            plainText = plainText.ToUpper();    
+            for(int i = 0; i < plainText.Length;i++)
+            {
+                int row_no = Math.Abs((int)('A') - key[Key_counter]);
+                int col_no = Math.Abs((int)('A') - plainText[i]);
+                cipherText += (char)letters[row_no,col_no];
+                Key_counter++;
+            }
+            
+
+            return cipherText.ToUpper();
         }
     }
 }
