cf487a67aeb8147e253deec5d5544673acdf3926
Abu3rida <khaledabuarida0@gmail.com>
2023-03-13 15:46:02 +0200

    adding the RailFence.cs file by khaled

--- Milestone1and2/SecurityPackage[Template]/securitylibrary/MainAlgorithms/RailFence.cs
+++ Milestone1and2/SecurityPackage[Template]/securitylibrary/MainAlgorithms/RailFence.cs
@@ -10,17 +10,86 @@ namespace SecurityLibrary
     {
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            plainText = plainText.ToLower();
+            cipherText = cipherText.ToLower();
+            int key = 1;
+            for(int i=0; i < 100; i++)
+            {
+                string myCipher = Encrypt(plainText, key);
+                if(String.Compare(myCipher,cipherText) == 0)
+                {
+                    break;
+                }
+                key++;
+            }
+            return key;
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            decimal x = key;
+            decimal y = Math.Ceiling(cipherText.Length / x);
+            char[,] board = new char[(int)x, (int)y];
+            int index = 0;
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    if (index != cipherText.Length)
+                    {
+                        board[i, j] = cipherText[index];
+                        index++;
+                    }
+                }
+            }
+
+            string plainText = "";
+            for (int j = 0; j < y; j++)
+            {
+                for (int i = 0; i < x; i++)
+                {
+                    plainText += board[i, j];
+                }
+            }
+            return plainText;
+
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            //preparing the dimentions of the matrix
+            decimal x = key;
+            decimal y = Math.Ceiling(plainText.Length / x);
+
+            //initialize the matrix
+            char[,] board = new char[(int)x, (int)y];
+
+            int index = 0;
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    if (index != plainText.Length)
+                    {
+                        board[j, i] = plainText[index];
+                        index++;
+                    }
+                }
+            }
+            string cipherText = "";
+            for (int j = 0; j < x; j++)
+            {
+                for (int i = 0; i < y; i++)
+                {
+                    cipherText = cipherText + board[j, i];
+                }
+            }
+            return cipherText;
         }
     }
 }
