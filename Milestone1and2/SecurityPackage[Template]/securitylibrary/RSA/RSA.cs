708d2ecf0968812aff1133b9d7372844a466ade6
Mariamx811 <mariam.ahmed.xx24@gmail.com>
2023-05-05 02:01:21 +0300

    RSA Enc & Dec

--- Milestone1and2/SecurityPackage[Template]/securitylibrary/RSA/RSA.cs
+++ Milestone1and2/SecurityPackage[Template]/securitylibrary/RSA/RSA.cs
@@ -10,12 +10,42 @@ namespace SecurityLibrary.RSA
     {
         public int Encrypt(int p, int q, int M, int e)
         {
-            throw new NotImplementedException();
+            double n = p * q;
+
+
+            double cipher = power(M, e, (int)n);
+
+
+            return (int)cipher;
         }
 
         public int Decrypt(int p, int q, int C, int e)
         {
-            throw new NotImplementedException();
+            double n = (p - 1) * (q - 1);
+            double nTmp = p * q;
+            int d = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if ((i * e) % n == 1)
+                {
+                    d = i;
+                    break;
+                }
+            }
+            double cipher = power(C, d, (int)nTmp);
+
+
+            return (int)cipher;
+        }
+        public static long power(int Text, int Key, int M)
+        {
+
+            if (Key == 0) return 1;
+            long res = power(Text, Key / 2, M);
+            res = (res * res) % M;
+            if (Key % 2 == 1) res = (res * (Text % M)) % M;
+            return res;
         }
     }
 }
